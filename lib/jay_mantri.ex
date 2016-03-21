2b7f94febd281d86e6d8f0f4a26d641e0665ed9e
Forest Loren Toney III <foresttoney@gmail.com>
2016-03-21 14:04:19 -0700

    fix: Add match for nil in find_next_url

--- lib/jay_mantri.ex
+++ lib/jay_mantri.ex
@@ -2,9 +2,6 @@ defmodule JayMantri do
 
   @moduledoc """
   """
-
-  require IEx
-
   @site_url "http://jaymantri.com"
 
   def get_content(html) do
@@ -18,6 +15,7 @@ defmodule JayMantri do
     |> hd
   end
 
+  # TODO - Determine is this is the approperiate way to pattern match next_url
   def get_next_url(html) do
     next_url = html
     |> Floki.find(".nextPage")
@@ -25,7 +23,7 @@ defmodule JayMantri do
 
     case next_url do
       [url] -> combine_url(@site_url, url)
-      _     ->
+      _     -> nil
     end
   end
 
@@ -47,11 +45,13 @@ defmodule JayMantri do
     IO.inspect(html)
   end
 
+  # TODO - Determine if this is the appropriate way to pattern match process_url
   defp process_url(url) do
     %HTTPoison.Response{:body => html} = HTTPoison.get!(url)
 
     case get_next_url(html) do
-      url -> process_url(url)
+      url when is_bitstring(url) -> process_url(url)
+      _                          -> nil
     end
   end
 
