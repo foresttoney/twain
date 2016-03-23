92e6804d133fe7e27a61bbf2d671a9124b245e43
Forest Loren Toney III <foresttoney@gmail.com>
2016-03-22 23:46:20 -0700

    refactor: Refactor to use Streams and use more idiomatic Elixir

--- lib/jay_mantri.ex
+++ lib/jay_mantri.ex
@@ -1,70 +1,77 @@
-defmodule JayMantri do
+defmodule Twain.JayMantri do
 
   @moduledoc """
+  Scrape [`Jay Mantri`](http://jaymantri.com/) for all images and corresponding tags.
   """
 
-  @site_url "http://jaymantri.com"
+  @type html :: String.t
+  @type url  :: String.t
 
-  def get_content(html) do
+  @url "http://jaymantri.com"
+
+  def scrape do
+    stream = Stream.resource(
+      fn        -> start(@url) end,
+      fn(state) -> process(state) end,
+      fn        -> {:complete} end
+    )
+
+    Stream.run(stream)
+  end
+
+  @spec get_content(html) :: Floki.html_tree
+  defp get_content(html) do
     Floki.find(html, "#posts .post")
   end
 
-  def get_image_url(html) do
-    with [anchor|_]          <- Floki.find(html, "div.caption p a"),
-         [href|_]            <- Floki.attribute(anchor, "href"),
-         decoded_url         <- URI.decode(href),
+  @spec get_full_url(url, String.t) :: url
+  defp get_full_url(url, file_path) do
+    url <> file_path
+  end
+
+  @spec get_image_url(Floki.html_tree) :: url
+  def get_image_url(content) do
+    with [a]                 <- Floki.find(content, "div.caption p a"),
+         [href]              <- Floki.attribute(a, "href"),
+         decoded_url          = URI.decode(href),
          %{:query => query}  <- URI.parse(decoded_url),
          %{"z" => image_url} <- URI.decode_query(query),
       do: image_url
   end
 
-  # TODO - Determine is this is the approperiate way to pattern match next_url
-  def get_next_url(html) do
-    next_url = html
+  @spec get_next_page_url(html) :: url | nil
+  defp get_next_page_url(html) do
+    next_page_url = html
     |> Floki.find(".nextPage")
     |> Floki.attribute("href")
 
-    case next_url do
-      [url] -> combine_url(@site_url, url)
-      _     -> nil
+    case next_page_url do
+      [file_path] -> get_full_url(@url, file_path)
+      _           -> nil
     end
   end
 
-  def get_tags(html) do
-    html
+  @spec get_tags(Floki.html_tree) :: [String.t]
+  defp get_tags(content) do
+    content
     |> Floki.find("ul.tags li a")
     |> Enum.map(&Floki.text/1)
   end
 
-  def scrape do
-    process_url(@site_url)
+  # defp parse(content) do
+  #   tags = Enum.map(content, &get_tags/1)
+  # end
+
+  defp process(state) do
+    {:halt, nil}
   end
 
-  defp combine_url(site_url, url) do
-    site_url <> url
-  end
-
-  defp parse(html) do
+  @spec start(url) :: {Floki.html_tree, url|nil}
+  defp start(url) do
+    %{:body => html} = HTTPoison.get!(@url)
     content = get_content(html)
-
-    images = content
-    |> Enum.map(&get_image_url/1)
-    |> Enum.map(&Content.get_image/1)
-
-    IO.inspect(images)
-
-    tags = get_tags(content)
-  end
-
-  # TODO - Determine if this is the appropriate way to pattern match process_url
-  defp process_url(url) do
-    %HTTPoison.Response{:body => html} = HTTPoison.get!(url)
-    parse(html)
-
-    # case get_next_url(html) do
-    #   url when is_bitstring(url) -> process_url(url)
-    #   _                          -> nil
-    # end
+    next_page_url = get_next_page_url(html)
+    {content, next_page_url}
   end
 
 end
